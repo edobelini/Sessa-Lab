5db555f2c27873fe1b59ed991eb6f5cc55a3635c
Mattia Zaghi <56716402+MattiaZaghi@users.noreply.github.com>
2023-05-12 15:21:53 +0200

    Update Fig.6-Supplementary Fig.8,9.R

--- Balanced_SET_levels_favor_the_correct_enhancer_repertoire_during_cell_fate_acquisition/Figures/Fig.6-Supplementary Fig.8,9.R
+++ Balanced_SET_levels_favor_the_correct_enhancer_repertoire_during_cell_fate_acquisition/Figures/Fig.6-Supplementary Fig.8,9.R
@@ -16,165 +16,15 @@ options(future.globals.maxSize = 8000 * 1024^2)
 
 setwd("./")
 
-data <- Read10X(data.dir = "./filtered_feature_bc_matrix/")
-
-Multi.object <- CreateSeuratObject(counts = data$`Gene Expression`, project = "RNA", min.cells = 3, min.features = 200)
-rm(data)
-
-Multi.object$Condition <- as.data.frame(str_split_fixed(rownames(Multi.object@meta.data), pattern = "-", n = 2))[,2]
-Multi.object@meta.data["Condition"][Multi.object@meta.data["Condition"] == "1"] <- "embryo_mut"
-Multi.object@meta.data["Condition"][Multi.object@meta.data["Condition"] == "2"] <- "embryo_ctrl"
-Multi.object@meta.data["Condition"][Multi.object@meta.data["Condition"] == "2"] <- "P2_ctrl"
-Multi.object@meta.data["Condition"][Multi.object@meta.data["Condition"] == "2"] <- "P2_mut"
-Multi.object$Genotype <- str_split_fixed(Multi.object@meta.data$Condition, "_", 2)[,2]
-
-dir.create("plots/")
-# The [[ operator can add columns to object metadata. This is a great place to stash QC stats
-Multi.object[["percent.mt"]] <- PercentageFeatureSet(Multi.object, pattern = "^mt-")
-VlnPlot(Multi.object, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3, log = F, group.by = "Condition")+
-  ggsave("plots/vio_plot.png", dpi = 330, scale = 0.8, width = 12, height = 5)
-
-Multi.object <- NormalizeData(Multi.object, normalization.method = "LogNormalize", scale.factor = 10000) %>%
-  FindVariableFeatures(selection.method = "vst", nfeatures = 2000)
-
-all.genes <- rownames(Multi.object)
-Multi.object <- ScaleData(Multi.object, features = all.genes)
+#Fig. 6 a UMAP plot of Gene Expression and chromatin accessibility
 
 
-Multi.object <- RunPCA(Multi.object, features = VariableFeatures(object = Multi.object)) %>% 
-    JackStraw(Multi.object, num.replicate = 100) %>%
-    ScoreJackStraw(Multi.object, dims = 1:10) %>%
-    ElbowPlot(Multi.object) %>%
-    FindNeighbors(Multi.object, dims = 1:20) %>%
-    FindClusters(Multi.object, resolution = 0.5) %>%
-    RunUMAP(Multi.object, dims = 1:15)
+Multiome <- readRDS("/home/zaghi/Documents/scRNA_Multiome/MultiOme.rds") #load single cell gene expression object from Seurat
 
-mycol <- kelly(11)
-mycol[1] <- "brown"
+scATAC_Multiome <- loadArchRProject("Documents/scATAC_Multiome_Bam/scATAC_multiome_ArchR/") #load ArchR object for single cell chromatin accessibility
 
-p1 = DimPlot(Multi.object, reduction = "umap", label = F, cols = mycol, pt.size = 1.5) +
-  theme(plot.title = element_text(color = "black", size = 10, face = "bold.italic"),
-        axis.text.x = element_text(angle = 90, face = "bold", color = "black", size=20, hjust =1),
-        axis.title.x = element_text(face = "bold", color = "black", size = 20),
-        axis.text.y = element_text(angle = 0, face = "bold", color = "black", size=20),
-        axis.title.y = element_text(face = "bold", color = "black", size = 0),
-        legend.text = element_text(face = "bold", color = "black", size = 20),
-        legend.position="top",
-        panel.background = element_rect(fill = "white",colour = "black", size = 1, linetype = "solid")) +
-  labs(x = "UMAP 1", y = "UMAP 2")+
-  ggtitle("UMAP 1:15, FindNeighbors 1:20")
-p1
+#transfer single cell ATAC UMAP coordinates in Seurat object to plot UMAP
 
-p2 = DimPlot(Multi.object , reduction = "umap", group.by = "Condition", label = F, pt.size = 0.1, cols = c("red", "blue", "green", "black"))+
-  theme(plot.title = element_text(color="black", size=20, face="bold.italic"),
-        axis.text.x = element_text(angle = 90, face = "bold", color = "black", size=20, hjust =1),
-        axis.title.x = element_text(face = "bold", color = "black", size = 20),
-        axis.text.y = element_text(angle = 0, face = "bold", color = "black", size=20),
-        axis.title.y = element_text(face = "bold", color = "black", size = 20),
-        legend.text = element_text(face = "bold", color = "black", size = 10),
-        legend.position="left",
-        panel.background = element_rect(fill = "white",colour = "black", size = 1, linetype = "solid")) +
-  labs(x = "UMAP 1", y = "UMAP 2")
-p2
-
-png("plots/UMAP.png", width = 5, height = 10, units = "cm", res = 330, pointsize = 3)
-p1+p2
-dev.off()
-
-bench_in <- c("nCount_RNA", "nFeature_RNA", "percent.mt")
-bench_plot_list <- list()
-for (i in bench_in) {
-  FeaturePlot(Multi.object, features = c(i))+
-    scale_colour_gradientn(colours = viridis(20))
-  ggsave(paste("plots/",i ,".png"), dpi = 330, scale = 0.8, width = 12, height = 5)
-}
-
-thresh.use = 0.25
-min.pct = 0.25
-min.diff.pct = -Inf
-test.use = "wilcox"
-LogFC.onlypos = TRUE
-p_val_adj_T = 0.05
-
-cluster.markers <- FindAllMarkers(Multi.object,
-                                 thresh.use = thresh.use,
-                                 test.use = test.use,
-                                 min.pct = min.pct,
-                                 min.diff.pct = min.diff.pct,
-                                 only.pos = LogFC.onlypos) %>%
-                    as.data.frame() %>%
-                    filter(p_val_adj < p_val_adj_T)
-
-top100 <- cluster.markers %>%
-group_by(cluster) %>%
-top_n(n = 100, wt = avg_log2FC) %>%
-as.data.frame()
-rownames(top100) <- top100$gene
-
-MG_list <- list()
-
-for (i in 1:length(unique(Idents(Multi.object)))) {
-  MG_list[[i]] <- filter(top100, top100$cluster == i)
-  names(MG_list)[i] <- paste("Cluster", i, sep = "_")
-}
-
-openxlsx::write.xlsx(MG_list, "Marker_Genes_100.xlsx")
-
-new.cluster.name <- c("ExN_UL", "OPCs_Oligo", "Astro", "IN", "Astro", "INP", "AP_RGC", "Late_Prog", "ExN_DL", 
-  "Microglia", "ExN_UL", "Endothelium", "Endothelium", "Astro", "ExN_DL", "Late_Prog")
-
-
-
-DEGS <- list()
-Idents(Multi.object) <- Multi.object$Genotype
-
-for (i in as.vector(unique(Multi.object$Label_cluster))) {
-  filt <-  subset(Multi.object, subset = Label_cluster %in% i)
-  DGE <- FindMarkers(filt, ident.1 = "mut" , ident.2 = "ctrl", 
-                     logfc.threshold = 0.25, test.use = "wilcox")
-  DEGS[[i]] <- as.data.frame(DGE)
-  DEGS[[i]]$SYMBOL <- rownames(DEGS[[i]])
-  names(DEGS)[i] <- paste("DEGS", i, sep = "_")
-}
-
-openxlsx::write.xlsx(MG_list, "DEGS_intra_cluster.xlsx")
-
-A <- as.data.frame(prop.table(table(Idents(Multi.object), Multi.object$Condition), margin = 2))
-A2 <- as.data.frame(table(Idents(Multi.object), Multi.object$Condition), margin = 2)
-A$N <- A2$Freq
-
-
-M <- ggplot(data = A, aes(x = Var2, y = Freq, fill = Var1)) +
-  geom_bar(stat = "identity",  color = "black", position = "fill") +
-  theme(plot.title = element_text(color = "black", size = 16, face = "bold.italic"),
-        axis.text.x = element_text(angle = 45, face = "bold", color = "black", size = 12, hjust = 1), 
-        axis.title.x = element_text(face = "bold", color = "black", size = 14),
-        axis.text.y = element_text(angle = 0, face = "bold", color = "black", size = 12),
-        axis.title.y = element_text(face = "bold", color = "black", size = 14),
-        legend.title = element_text(size = 0),
-        legend.text = element_text(face = "bold", color = "black", size = 12),
-        legend.position = "left",
-        panel.background = element_rect(fill = "transparent", colour = "black", size = 1, linetype = "solid")) +
-  labs(x = "", y = "fraction of cells")+
-  scale_fill_manual(values = col)
-
-png("plots/Perc_Condition_in_clusters_A.png",
-    width     = 6,
-    height    = 8,
-    units     = "in",
-    res       = 330)
-
-M
-dev.off()
-
-
-#Fig. 6 e Single cell Multiome UMAP based plotting on ATAC and RNA 
-
-#First step is to transfer scATAC coordinates into Seurat object
-
-scRNA_Multiome <- readRDS("Documents/scRNA_Multiome/MultiOme.rds") #load Seurat data set
-
-scATAC_Multiome <- loadArchRProject("Documents/scATAC_Multiome_Bam/scATAC_multiome_ArchR_correct/") #load ArchR project
 
 barcodes_scATAC <- as.data.frame(scATAC_Multiome@embeddings@listData[["UMAP"]]@listData[["df"]]) %>% 
   rownames_to_column() %>% 
@@ -206,45 +56,85 @@ barcodes_scATAC <- barcodes_scATAC %>%
 barcodes_scATAC <- barcodes_scATAC[order(rownames(barcodes)),]
 barcodes_scATAC <- barcodes_scATAC[rownames(Multihome@meta.data),]
 
-scRNA_Multiome[["cloupe"]] <- CreateDimReducObject(embeddings = scRNA_Multiome[["umap"]]@cell.embeddings, assay = DefaultAssay(scRNA_Multiome),
+Multihome[["ATAC"]] <- CreateDimReducObject(embeddings = Multihome[["umap"]]@cell.embeddings, assay = DefaultAssay(Multihome),
                                              key = "cloupe_")
 
-scRNA_Multiome[["cloupe"]]@cell.embeddings[,1] <- barcodes_scATAC[,1]
-scRNA_Multiome[["cloupe"]]@cell.embeddings[,2] <- barcodes_scATAC[,2]
+Multihome[["ATAC"]]@cell.embeddings[,1] <- barcodes_scATAC[,1]
+Multihome[["ATAC"]]@cell.embeddings[,2] <- barcodes_scATAC[,2]
 
-#Plot single cells clusters into UMAP 
-
+#plot UMAP with clusters and samples for ATAC & RNA
 
 colors <- kelly(11)
-Idents(Multihome)
 colors[1] <- "brown" 
-p1 = DimPlot(Multihome, reduction = "cloupe", label = F, pt.size = 0.3, cols = colors,group.by="Condition") + #Plot ATAC UMAP
+p1 = DimPlot(Multihome, reduction = "ATAC", label = F, pt.size = 0.3, cols = colors,group.by="Label_cluster") + 
   theme_void()+
   theme(legend.text = element_text(color = "black", size=12))
 p1
 ggsave("SETBP1_epigenomics/pipeline/plots/UMAP_no_labels_ATAC.png", plot = last_plot(), device = NULL, path = NULL,
        scale = 1, width = 200, height = 200, units = "mm", dpi = 300, limitsize = TRUE)
+  p1+theme(plot.title = element_text(color="black", size=7, face="bold.italic"),
+        axis.text.x = element_text(angle = 90, face = "bold", color = "black", size=5, hjust =1), 
+        axis.title.x = element_text(face = "bold", color = "black", size = 5),
+        axis.text.y = element_text(angle = 0, face = "bold", color = "black", size=5),
+        axis.title.y = element_text(face = "bold", color = "black", size = 5),
+        legend.text = element_text(face = "bold", color = "black", size = 12),
+        legend.position="top", text = element_text(face = "bold", color = "black", size=5),
+        panel.background = element_rect(fill = "white",colour = "black", size = 1, linetype = "solid")) +
+  labs(x = "UMAP 1", y = "UMAP 2")
+  
+  colors <- c("red","blue","darkgreen","purple")
+  p1 = DimPlot(Multihome, reduction = "ATAC", label = F, pt.size = 0.3, cols = colors,group.by="Condition") + 
+    theme_void()+
+    theme(legend.text = element_text(color = "black", size=12))
+  p1
+  ggsave("SETBP1_epigenomics/pipeline/plots/UMAP_no_labels_ATAC_samples.png", plot = last_plot(), device = NULL, path = NULL,
+         scale = 1, width = 200, height = 200, units = "mm", dpi = 300, limitsize = TRUE)
+  p1+theme(plot.title = element_text(color="black", size=7, face="bold.italic"),
+           axis.text.x = element_text(angle = 90, face = "bold", color = "black", size=5, hjust =1), 
+           axis.title.x = element_text(face = "bold", color = "black", size = 5),
+           axis.text.y = element_text(angle = 0, face = "bold", color = "black", size=5),
+           axis.title.y = element_text(face = "bold", color = "black", size = 5),
+           legend.text = element_text(face = "bold", color = "black", size = 12),
+           legend.position="top", text = element_text(face = "bold", color = "black", size=5),
+           panel.background = element_rect(fill = "white",colour = "black", size = 1, linetype = "solid")) +
+    labs(x = "UMAP 1", y = "UMAP 2")
 
-
-  p1 = DimPlot(Multihome, reduction = "umap", label = F, pt.size = 0.3, cols = colors,group.by="Condition") + #Plot ATAC UMAP
+colors <- kelly(11)
+colors[1] <- "brown"
+  p1 = DimPlot(Multihome, reduction = "umap", label = F, pt.size = 0.3, cols = colors,group.by="Label_cluster") + 
     theme_void()+
     theme(legend.text = element_text(color = "black", size=12))
   p1
   ggsave("SETBP1_epigenomics/pipeline/plots/UMAP_no_labels_RNA.png", plot = last_plot(), device = NULL, path = NULL,
          scale = 1, width = 200, height = 200, units = "mm", dpi = 300, limitsize = TRUE)
-
-
-#Fig. 6 g chromatin accessibility heatmap based on pseudotime of AP_RGC to ExN_DL differentiation trajectory
-
-
-#First step is to add the peaks associated to the specific developemental trajectory to control and mutant cells
-
-
-
-
-
-
-
+  p1+theme(plot.title = element_text(color="black", size=7, face="bold.italic"),
+           axis.text.x = element_text(angle = 90, face = "bold", color = "black", size=5, hjust =1), 
+           axis.title.x = element_text(face = "bold", color = "black", size = 5),
+           axis.text.y = element_text(angle = 0, face = "bold", color = "black", size=5),
+           axis.title.y = element_text(face = "bold", color = "black", size = 5),
+           legend.text = element_text(face = "bold", color = "black", size = 12),
+           legend.position="top", text = element_text(face = "bold", color = "black", size=5),
+           panel.background = element_rect(fill = "white",colour = "black", size = 1, linetype = "solid")) +
+    labs(x = "UMAP 1", y = "UMAP 2")
+  
+  
+  colors <- c("red","blue","darkgreen","purple")
+  p1 = DimPlot(Multihome, reduction = "umap", label = F, pt.size = 0.3, cols = colors,group.by="Condition") + 
+    theme_void()+
+    theme(legend.text = element_text(color = "black", size=12))
+  p1
+  ggsave("SETBP1_epigenomics/pipeline/plots/UMAP_no_labels_RNA_samples.png", plot = last_plot(), device = NULL, path = NULL,
+         scale = 1, width = 200, height = 200, units = "mm", dpi = 300, limitsize = TRUE)
+  p1+theme(plot.title = element_text(color="black", size=7, face="bold.italic"),
+           axis.text.x = element_text(angle = 90, face = "bold", color = "black", size=5, hjust =1), 
+           axis.title.x = element_text(face = "bold", color = "black", size = 5),
+           axis.text.y = element_text(angle = 0, face = "bold", color = "black", size=5),
+           axis.title.y = element_text(face = "bold", color = "black", size = 5),
+           legend.text = element_text(face = "bold", color = "black", size = 12),
+           legend.position="top", text = element_text(face = "bold", color = "black", size=5),
+           panel.background = element_rect(fill = "white",colour = "black", size = 1, linetype = "solid")) +
+    labs(x = "UMAP 1", y = "UMAP 2")
+  
 
 
 
